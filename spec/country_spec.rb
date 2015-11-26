2ebb1f8b65391e5ab1bac1e4eb08d4636ef6ae0b
Oleh Novosad <oleh.novosad@gmail.com>
2015-11-26 15:30:51 -0800

    Renamed Country to CountryAtWork to avoid global conflicts

--- spec/country_spec.rb
+++ spec/country_spec.rb
@@ -321,7 +321,7 @@ describe ISO3166AtWork::Country do
 
   describe 'Country class' do
     context "when loaded via 'iso3166' existance" do
-      subject { defined?(Country) }
+      subject { defined?(CountryAtWork) }
 
       it { is_expected.to be_falsey }
     end
@@ -330,20 +330,20 @@ describe ISO3166AtWork::Country do
       before { require 'countries/global' }
 
       describe 'existance' do
-        subject { defined?(Country) }
+        subject { defined?(CountryAtWork) }
 
         it { is_expected.to be_truthy }
       end
 
       describe 'superclass' do
-        subject { Country.superclass }
+        subject { CountryAtWork.superclass }
 
         it { is_expected.to eq(ISO3166AtWork::Country) }
       end
 
       describe 'to_s' do
         it 'should return the country name' do
-          expect(Country.new('GB').to_s).to eq('United Kingdom')
+          expect(CountryAtWork.new('GB').to_s).to eq('United Kingdom')
         end
       end
     end
